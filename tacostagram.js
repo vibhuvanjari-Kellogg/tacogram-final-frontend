2e2b02334c261143d044594ceaaabcac01d6b1af
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-03-11 19:25:37 +0000

    first break

--- tacostagram.js
+++ tacostagram.js
@@ -1,6 +1,6 @@
 function getPostsFromAPI() {
   // Replace this with the URL of the JSON API that returns an array of image URLs
-  const url = 'YOUR_URL_GOES_HERE';
+  const url = 'https://3000-vibhuvanjar-tacogramfin-4i97wr155i0.ws-us118.gitpod.io/posts.json';
   if (url == 'YOUR_URL_GOES_HERE') {
     alert('Error: Replace url value in tacostagram.js')
   }
@@ -11,11 +11,16 @@ function getPostsFromAPI() {
     .then(posts => {
       // Loop through the array of posts and build html for each
       for (let post of posts) {
+        // alert(`got it`)
         // Log post data to browser console
         console.log(post);
 
         // html for each post goes here
-        let html = ``;
+        let html = `
+        <div class = "col-sm-6 col-sm-3">
+        <img src="${post["image"]}" class="img-fluid">
+        <p>${post["body"]}</p>
+        `;
 
         const postsDiv = document.querySelector('#posts');
         postsDiv.innerHTML += html
